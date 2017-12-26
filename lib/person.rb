007a2c82ff4b2b33f8a5d67bbab27db6fed2c732
Kathy Hudson <khitx@icloud.com>
2017-12-26 20:29:50 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -7,11 +7,13 @@ class Person
   end
   def initialize(name)
   @name = name
-end
-
-def name
-  @name
-end
+  end
+  def name
+    @name
+  end
+  def name=(new_name)
+    @name = new_name
+  end
 end
 
 kanye = Person.new("Kanye")
