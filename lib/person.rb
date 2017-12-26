adad0e567368561198600902d9ac65c0d1db88bb
Kathy Hudson <khitx@icloud.com>
2017-12-26 20:30:43 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -14,6 +14,18 @@ class Person
   def name=(new_name)
     @name = new_name
   end
+  def initialize(first_name, last_name)
+  @first_name = first_name
+  @last_name = last_name
+  end
+  def name=(full_name)
+    first_name, last_name = full_name.split
+    @f irst_name = first_name
+    @last_name = last_name
+  end
+  def name
+    "#{@first_name} #{@last_name}".strip
+  end 
 end
 
 kanye = Person.new("Kanye")
