b10b3b3f1dd4934c3cb4a073dd3c3ce01f0edad1
Kathy Hudson <khitx@icloud.com>
2017-12-26 20:16:29 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -5,4 +5,14 @@ class Person
   def walk
     puts("The Person is walking")
   end
+  def initialize(name)
+  @name = name
 end
+
+def name
+  @name
+end
+end
+
+kanye = Person.new("Kanye")
+kanye.name
