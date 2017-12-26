27dfdfe08007d3241a2f184c20ea8f9b03b52ed1
Kathy Hudson <khitx@icloud.com>
2017-12-26 20:09:27 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,4 +5,14 @@ class Dog
   def sit
     puts('The Dog is sitting')
   end
+  def name=(dog_name)
+    this_dogs_name = dog_name
+  end
+  def name
+    this_dogs_name
+  end
 end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+lassie.name
